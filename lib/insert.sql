4337ab762a710c9f0c3b212d7d8c44562b24d7ba
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-13 17:26:23 -0400

    tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 15, 'male', 'brown', 'very nice', 1),
+    ('Rowdy', 4, 'male', 'black','playful', 1),
+    ('Tabitha', 6,'female', 'grizzly', 'protective', 1),
+    ('Sergeant Brown', 10, 'male','brown', 'well behaved', 1),
+    ('Melissa', 2, 'female','black','quiet', 1),
+    ('Grinch',7,'male','polar','steals things', 1),
+    ('Wendy', 12, 'female', 'panda','sweet', 1),
+    (NULL, NULL, 'male','brown',NULL, 0)
+
